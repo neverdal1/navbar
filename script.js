b3c47860777980831a0f02c45e1f00d78aaf6ffc
neverdal1 <neverdalarild@gmail.com>
2024-04-02 12:03:38 +0200

    js.css.html combined

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello!');
+
+const hamburgerIcon = document.querySelector("#hamburgerIcon")
+console.log(hamburgerIcon)
+
+function hamburgermeny(){
+    const nav = document.querySelector("#nav")
+    console.log(nav)
+    nav.classList.toggle("headerhamburger")
+    console.log("clicked")
+    
+}
+
+hamburgerIcon.addEventListener("click", hamburgermeny)
+
+
+
