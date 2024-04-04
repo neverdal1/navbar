5b7aa51ae1ef792dcec041df8d84b63957838216
neverdal1 <neverdalarild@gmail.com>
2024-04-04 11:55:09 +0200

    js const lagmodsend

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ hamburgerIcon.addEventListener("click", hamburgermeny)
 
 
 
+const listname = document.querySelector("#listname");
+console.log(listname); 
+
+
+//lager element i html 
+const  lihjem = document.createElement("li");
+lihjem.classList.add("listitam" )
+
+const liprosjekter = document.createElement("li");
+liprosjekter.classList.add("listitam" )
+
+const likontakt = document.createElement("li");
+likontakt.classList.add("listitam" )
+
+const  liomoss = document.createElement("li");
+liomoss.classList.add("listitam" )
+
+
+
+//modifiserer synlig i html
+lihjem.textContent = "hjem"
+liprosjekter.textContent = "prosjekter"
+likontakt.textContent = "kontakt"
+liomoss.textContent = 'om oss'
+
+// sendt inn i html
+listname.appendChild(lihjem);
+listname.appendChild(likontakt);
+listname.appendChild(liprosjekter);
+listname.appendChild(liomoss)
+
+
+
+
+
